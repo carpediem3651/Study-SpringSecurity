a1f49a998406a4ab88f441d13146f15e7990d454
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-30 12:42:11 +0900

    세션 사용자 아이디 정보

--- src/main/java/com/example/TestSecurity/controller/MainController.java
+++ src/main/java/com/example/TestSecurity/controller/MainController.java
@@ -1,13 +1,34 @@
 package com.example.TestSecurity.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping
-    public String mainP() {
+    public String mainP(Model model) {
+        // id 정보 얻기
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // role정보 얻기
+        Authentication authentication = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = ((org.springframework.security.core.Authentication) authentication).getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        
+        model.addAttribute("id", id);
+        model.addAttribute("role",role);
+
         return "main";
     }
 }
